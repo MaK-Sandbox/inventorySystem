e07d75a575cc628bf48ba5c5d9651f97ed981de4
Maria T. Andersen <mteilmann93@gmail.com>
2025-07-11 20:51:09 +0200

    first of all, the path passed to new Database is considered from the root, not the local file where the database object is created. Second, refer to our DB_file as defined in our environment variables

--- src/db.js
+++ src/db.js
@@ -1,9 +1,15 @@
 import Database from "better-sqlite3";
 
+if (!process.env.DB_FILE) {
+  console.error("DB_FILE entry missing in environment");
+  process.exit(1);
+}
+
 let db;
 
 if (!db) {
-  db = new Database("../inventory.sqlite");
+  db = new Database(process.env.DB_FILE);
+  db.pragma("foreign_keys = ON");
 }
 
 export default db;
