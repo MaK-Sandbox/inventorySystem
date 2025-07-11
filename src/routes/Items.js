e557ee31b936343fb989ebda7f0a1a9f78a760fb
Maria T. Andersen <mteilmann93@gmail.com>
2025-07-11 20:52:36 +0200

    adapt post request according to new changes to the items table and test before committing that everything works

--- src/routes/Items.js
+++ src/routes/Items.js
@@ -5,8 +5,7 @@ import db from "../db.js";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  const stmt = db.prepare("SELECT * FROM items");
-  const items = stmt.all();
+  const items = db.prepare("SELECT * FROM items").all();
   res.send(items);
 });
 
@@ -28,39 +27,60 @@ router.get("/:id", (req, res) => {
 });
 
 router.post("/", (req, res) => {
-  const name = req.body["name"];
-  const quantity = req.body["quantity"];
-  const locationId = req.body["location_id"];
-  const purchasePrice = req.body["purchase_price"];
-  const purchaseDate = req.body["purchase_date"];
-  const receipt = req.body["receipt"];
-  const freeText = req.body["freeText"];
+  // Validate properties
+  const allowedProperties = [
+    "name",
+    "quantity",
+    "location_id",
+    "purchase_price",
+    "currency_id",
+    "purchase_date",
+    "freeText",
+  ];
+
+  // Step 1: Check if a request body was provided. If not, return an error message back to the user
+  if (Object.keys(req.body).length === 0) {
+    res.status(400).send("Provide a body to put request");
+    return;
+  }
+
+  // Step 2: Validate and filter properties
+  const insertData = {};
+  for (const key in req.body) {
+    if (Object.prototype.hasOwnProperty.call(req.body, key)) {
+      const value = req.body[key];
+
+      // If an illegal property is detected, return an error message to the user
+      if (!allowedProperties.includes(key)) {
+        res.status(400).send("Illegal property");
+        return;
+      }
+      insertData[key] = value;
+    }
+  }
+
+  // Step 3: Use properties of validated entries and values
+  const validatedProperties = Object.keys(insertData);
+  const validatedValues = Object.values(insertData);
 
   const insertStatement = db.prepare(
-    "INSERT INTO items (name, quantity, location_id, purchase_price, purchase_date, receipt, freeText) VALUES (?, ?, ?, ?, ?, ?, ?)"
+    `INSERT INTO items (${validatedProperties.join(
+      ", "
+    )}) VALUES (?, ?, ?, ?, ?, ?, ?)`
   );
 
-  const info = insertStatement.run(
-    name,
-    quantity,
-    locationId,
-    purchasePrice,
-    purchaseDate,
-    receipt,
-    freeText
-  );
+  const info = insertStatement.run(...validatedValues);
 
   // Assuming that the creation was successful, store the id of the lastest inserted row
-
   if (info.changes === 0) {
     res.send(403).send("Forbidden");
     return;
   }
 
-  const newEmployeeId = info.lastInsertRowid;
+  const newItemId = info.lastInsertRowid;
 
   // Return the newly created item
-  res.json(db.prepare("SELECT * FROM items WHERE id = ?").get(newEmployeeId));
+  res.json(db.prepare("SELECT * FROM items WHERE id = ?").get(newItemId));
 });
 
 router.put("/:id", (req, res) => {
