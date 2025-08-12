be9d910a6f5e64158af8fbdfb1490f2d315fdca0
Maria T. Andersen <mteilmann93@gmail.com>
2025-08-12 22:11:50 +0200

    command 'tsc' run at root level. No errors. We have officially migrated converted the backend from javascript to typescript

---
+++ built/server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors";
+import fileUpload from "express-fileupload";
+// Initialize Express app
+const app = express();
+const port = process.env.PORT || 3000;
+let domain;
+if (process.env.NODE_ENV === "development") {
+    domain = "127.0.0.1";
+}
+else {
+    domain = "ser6pro";
+}
+// Use JSON middleware
+app.use(express.json());
+app.use(express.static("public"));
+app.use(cors());
+app.use(fileUpload());
+// Import and initialize routers
+import itemsRouter from "./routes/Items.js";
+import locationsRouter from "./routes/Locations.js";
+import searchRouter from "./routes/Search.js";
+import documentsRouter from "./routes/Documents.js";
+// Use imported routers
+app.use("/api/v1/items", itemsRouter);
+app.use("/api/v1/locations", locationsRouter);
+app.use("/search", searchRouter);
+app.use("/api/v1/documents", documentsRouter);
+app.get("/", (req, res) => {
+    res.send("Test route");
+});
+app.listen(port, () => {
+    console.log(`App listening at http://${domain}:${port}`);
+});
