7dbec3fd63742cf28d587b94c8cabcc694353601
Maria T. Andersen <mteilmann93@gmail.com>
2025-08-13 15:13:06 +0200

    check whether we are in development by checking if the location hostname is localhost

--- public/test.js
+++ public/test.js
@@ -4,11 +4,15 @@ const itemsBtn = document.getElementById("items_btn");
 const addItemBtn = document.getElementById("add_item_btn");
 const documentsBtn = document.getElementById("documents_btn");
 
+// check if in devleopment
+const isDev = ["localhost", "127.0.0.1"].includes(window.location.hostname);
+const API_BASE_URL = isDev ? "http://localhost:3000" : "http://ser6pro:3000";
+
 // get #main_content
 const mainContent = document.getElementById("main_content");
 
 // show by default when page loads for the first time
-window.addEventListener("load", (event) => {
+window.addEventListener("load", () => {
   mainContent.textContent = "I just loaded!";
 });
 
